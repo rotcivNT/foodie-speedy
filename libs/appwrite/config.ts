07c6dc9959139a6287e7ac0d71d7b7225ace1f2b
rotciv <ngocthang.devweb@gmail.com>
2024-09-09 13:17:30 +0700

    update readme/md

--- libs/appwrite/config.ts
+++ libs/appwrite/config.ts
@@ -1,7 +1,7 @@
 export const config = {
-  endpoint: "https://cloud.appwrite.io/v1",
-  platform: "com.fs.foodie-speedy",
-  projectId: "66d47a4f002feac4bd55",
-  databaseId: "66d47d05001d835a2029",
-  imageStorageId: "66d47efe003d14a7ea12",
+  endpoint: process.env.EXPO_PUBLIC_END_POINT,
+  platform: process.env.EXPO_PUBLIC_PLATFORM,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  databaseId: process.env.EXPO_PUBLIC_DATABASE_ID,
+  imageStorageId: process.env.EXPO_PUBLIC_IMAGE_STORAGE_ID,
 };
